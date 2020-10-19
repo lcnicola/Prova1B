29cf3019130dec8bffcf2f3f4d37697af598332d
Luiz Carlos Nicola <lcnicola@gmail.com>
2020-10-18 22:09:12 -0300

    segundo commit

--- js/fornecedores.js
+++ js/fornecedores.js
@@ -1,39 +1,44 @@
 const formulario = document.querySelector('#formularioFornecedores');
 const inputNome = document.querySelector('#nome');
 const inputEmail = document.querySelector('#email');
-const inputTelefone = document.querySelector('#telefone');
 const inputProduto = document.querySelector('#produto');
+const inputTelefone = document.querySelector('#telefone')
 const fornecedores = document.querySelector('#fornecedores');
 const mensagens = document.querySelector('#mensagens');
+const valid_email = []
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    if (inputNome.value === '' || inputEmail.value === '' || inputTelefone.value === '' || inputProduto.value === '') {
+    if (inputNome.value === '' || inputEmail.value === '' || inputProduto.value === '' || inputTelefone.value === '') {
         mensagens.style.display = 'block';
         mensagens.style.background = 'red';
         mensagens.innerHTML = 'Preencha todos os campos!';
-	    
-	} else {
+    } 
+
+    else if (valid_email.includes(inputEmail.value)==true) {
+        alert("Esse email já existe!")
+    }
+
+    
+    else {
+        valid_email.push(inputEmail.value)
         const li = document.createElement('li');
-        li.innerHTML = `Nome: ${inputNome.value} <br>Email: ${inputEmail.value} <br>Telefone: ${inputTelefone.value} <br>Produto: ${inputProduto.value}`;
+        li.innerHTML = `Nome: ${inputNome.value} <br>Email: ${inputEmail.value} <br>Produto: ${inputProduto.value} <br>Telefone: ${inputTelefone.value}`;
         li.addEventListener('click', (e) => {
             const deletar = confirm('Você realmente gostaria de deletar este fornecedor?');
 
             if (deletar) {
                 e.target.remove();
-            					
-			}
-	    })
-        
+            }
+        })
+
         fornecedores.appendChild(li);
         mensagens.style.display = 'none';
 
         inputNome.value = '';
         inputEmail.value = '';
-		inputTelefone.value = '';
         inputProduto.value = '';
+        inputTelefone.value = '';
     }
-
 })
-
